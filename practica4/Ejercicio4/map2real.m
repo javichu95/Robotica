3610e453b216c270beb8c58acc2623b88f9648f4
Grexoj <george95zgz@gmail.com>
2017-04-12 02:57:49 +0200

    Ficheros matlab borrados

--- practica4/Ejercicio4/map2real.m
+++
@@ -1,6 +0,0 @@
-function [ coordenadas ] = map2real( coordMap, mapa )
-    %Transforma coordenadas del mapa al mundo real.
-    coordenadas = [ coordMap(2)-1, size(mapa,1)-coordMap(1) ];
-    coordenadas = (coordenadas - 1)/2;
-end
-
